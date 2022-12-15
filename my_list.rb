a6709d1ed12b947388590137a43e3f9a11540284
Marcus Aurelius <99001924+MarcoAurelioAntonio@users.noreply.github.com>
2022-12-15 18:16:11 -0300

    Fix Offenses

--- my_list.rb
+++ my_list.rb
@@ -1,26 +1,22 @@
 require_relative 'my_enumerable'
 
 class Mylist
-    include My_enumerable
+  include MyEnumerable
 
-    def initialize(*list)
-        @list = list
-    end
+  def initialize(*list)
+    @list = list
+  end
 end
 
 list = Mylist.new(1, 2, 4, 3)
 
-p list 
+p list
 
-p list.all? {|e| e < 5}
-p list.all? {|e| e > 5}
+p(list.all? { |e| e < 5 })
+p(list.all? { |e| e > 5 })
 
-p list.any? {|e| e == 2}
-p list.any? {|e| e == 5}
+p(list.any? { |e| e == 2 })
+p(list.any? { |e| e == 5 })
 
-p list.filter {|e| e.even?}
-p list.filter {|e| e.odd?}
-
-# Optional MAX method
-p list.max_1
-p list.max_2(2)
+p list.filter(&:even?)
+p list.filter(&:odd?)
