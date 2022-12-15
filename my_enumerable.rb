fee665bee73a77e10e6ba3ec0959fcf21c007c71
joker69216 <hamaraad883@gmail.com>
2022-12-15 23:49:22 +0300

    delete the un needed code

--- my_enumerable.rb
+++ my_enumerable.rb
@@ -14,27 +14,9 @@ module My_enumerable
 
     def filter
         result = []
-        #for i in 0...@list.length
-        #    result << @list[i] if yield(@list[i])
-        #end
 
         @list.each {|e| result << e if yield(e)}
         result
     end
 
-    # Optional
-    def max_1
-        max = @list[0]
-        @list.each {|e| max = e if e > max}
-        max
-    end
-
-    #this is not the case. I need to return the n max elements
-    def max_2(n)
-        result = []
-        @list.each {|e| result << e if e > n}
-        result
-    end
-
-    
 end
